8290d46742f25ba8bc430c8ff60766c15b46a14d
bulaiocht <ko4erha@gmail.com>
2017-02-01 23:20:55 +0200

    MVC with Servlet and JSP
    Class which imitates retrieving student's data from some source (DB or
    other)

---
+++ servletdemo/src/com/luv2code/servletdemo/mvcstudent/StudentDataUtil.java
@@ -0,0 +1,19 @@
+package com.luv2code.servletdemo.mvcstudent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDataUtil {
+	public static List<Student> getStudents(){
+		//create empty list
+		List<Student> students = new ArrayList<>();
+		
+		//add STUB data
+		students.add(new Student("Michael", "Bae", "mich_bae@holly.com"));
+		students.add(new Student("Maria", "Gonzales", "m.gonzo@mail.com"));
+		students.add(new Student("Charles", "Glosterkwotch", "douches@westlberry.com"));
+		
+		//return list
+		return students;
+	}
+}
